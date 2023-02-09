ad74ec741255aab14b9c4ddccdfe039bc81c4bf4
feramdor <fernamdor99@gmail.com>
2023-02-09 17:39:10 -0600

    precios
    código de precios

--- main.py
+++ main.py
@@ -16,3 +16,19 @@ fechasCon=pd.to_datetime(["2021-01-29","2021-02-26","2021-03-31","2021-04-30","2
         "2021-11-30","2021-12-31","2022-01-26","2022-02-28","2022-03-31",
         "2022-04-29","2022-05-31","2022-06-30","2022-07-29","2022-08-31",
         "2022-09-30","2022-10-31","2022-11-30","2022-12-30","2023-01-25"])
+
+precios = fn.precios(
+    fn.ticker_reformat(tickers),
+    start_date=fechas_consulta[0],
+    end_date="2023-01-26",
+    fechas_consulta=fechas_consulta
+    )
+precios_cash = fn.precios(
+    fn.ticker_reformat(cash_tickers),
+    start_date=fechas_consulta[0],
+    end_date="2023-01-26",
+    fechas_consulta=fechas_consulta
+)
+precios_cash.drop("MXN",axis=1,inplace=True)
+pasiva_inicial = fn.pasiva_inicial(precios,precios_cash,tickers,pond,pond_cash,1e6)
+
