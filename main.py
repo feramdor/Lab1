5e66952246fff5fddbdd69fc094ba5f66a64eebb
feramdor <fernamdor99@gmail.com>
2023-02-09 17:33:59 -0600

    Modificación código
    Lectura files en main

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ Created on Tue Feb  7 21:09:51 2023
 
 @author: Fernanda
 """
+import functions as fn
+import data as dt
+import pandas as pd
+import numpy as np
 
+tickers, cash_tickers, pond, pond_cash = dt.tickers(dt.file_concat("files"))
