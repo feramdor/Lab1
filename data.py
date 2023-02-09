cd2146f49523d74e8f476db819c4d6fee9b59e54
feramdor <fernamdor99@gmail.com>
2023-02-09 16:37:09 -0600

    Nuevos files
    Se agregaron data y functions

---
+++ data.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 16:34:52 2023
+
+@author: Fernanda
+"""
+
---
+++ functions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 16:34:56 2023
+
+@author: Fernanda
+"""
+
